bdb12bad4cfd0e470b404a7abb4a4b3693874220
Alek Evans <gpg@alekeagle.com>
2022-04-11 08:52:05 -0600

    init rewrite

---
+++ electron/electron.js
@@ -0,0 +1,48 @@
+const { join } = require('path');
+const { app, BrowserWindow, Menu } = require('electron');
+
+const isDev =
+  process.env.IS_DEV === 'true' || process.env.NODE_ENV === 'development';
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: join(__dirname, 'preload.js')
+    },
+    icon: join(__dirname, '../build/icon.png')
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${join(__dirname, '../dist/index.html')}`
+  );
+
+  mainWindow.removeMenu();
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow.destroy();
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
